2f8cdd45fef6e422c3ed28570413719d3ef40745
JJ <jjbertran@lasalle.cat>
2020-11-15 19:07:28 +0100

    Added toast button

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configView()
+    }
+
+    private fun configView() {
+        findViewById<Button>(R.id.bt_toast).setOnClickListener {
+            Toast.makeText(applicationContext, "Hello!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
